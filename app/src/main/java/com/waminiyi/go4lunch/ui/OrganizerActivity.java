e0f93feba1b59cceb263611396142b57dd738a3a
Patrick SOTO <patrick.soto@diabeloop.fr>
2022-12-19 14:15:59 +0100

    G4L: Facebook sign in added

--- app/src/main/java/com/waminiyi/go4lunch/ui/OrganizerActivity.java
+++ app/src/main/java/com/waminiyi/go4lunch/ui/OrganizerActivity.java
@@ -2,7 +2,10 @@ package com.waminiyi.go4lunch.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.waminiyi.go4lunch.R;
 
@@ -12,5 +15,13 @@ public class OrganizerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organizer);
+
+        TextView tv=findViewById(R.id.tv);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(OrganizerActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
